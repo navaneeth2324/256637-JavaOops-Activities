5f6966245ed8c79853b4ab360d8cb9527d5d4209
navaneeth2324 <navneeth023@gmail.com>
2021-06-02 17:41:25 +0530

    Remaining solutions of Activity 1

---
+++ Activity1/InningsMain.java
@@ -0,0 +1,14 @@
+import java.util.9;
+class innings{
+    private String teamname,inningsname;
+    private int runs;
+
+    public String getTeamname(){
+        return teamname;
+    }
+}
+public class InningsMain{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+    }
+}
